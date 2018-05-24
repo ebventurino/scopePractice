2fe33f8a47846a1f1d4f5dc8e4aae13b674b853f
Emily Venturino <ebventurino@gmail.com>
2018-05-24 14:08:05 -0500

    finished challenge 2

--- scope.js
+++ scope.js
@@ -1,8 +1,21 @@
 
-const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+//                             Challenge 1
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
 
-for (let x = 0; x < cookies.length; x++) {
-    const currentCookie = cookies[x]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// for (let x = 0; x < cookies.length; x++) {
+//     const currentCookie = cookies[x]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
     
+// }
+
+//                             Challenge 2
+
+
+
+
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    console.log(conjoinedWord)
 }
+
+conjunction("Master", "Card")
