966247bf86d4ef8acf4201a7766ff3db29d78ed1
Emily Venturino <ebventurino@gmail.com>
2018-05-24 14:55:08 -0500

    started challenge 3

--- scope.js
+++ scope.js
@@ -13,9 +13,26 @@
 
 
 
-const conjunction = function (firstWord, secondWord) {
-    const conjoinedWord = `${firstWord} ${secondWord}`
-    console.log(conjoinedWord)
-}
+// const conjunction = function (firstWord, secondWord) {
+//     const conjoinedWord = `${firstWord} ${secondWord}`
+//     console.log(conjoinedWord)
+// }
 
-conjunction("Master", "Card")
+// conjunction("Master", "Card")
+
+//                          Challenge 3
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    }
+    const HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+    document.querySelector(".show-info").innerHTML = HTMLRepresentation
+    ModSquad.members.forEach((member) => {
+        let HTMLRepresentation = `<div>${member}</div>`
+    });
+}
